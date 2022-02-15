ead42506f9554029d2767e2fb50bddb49d024983
luciano <lucianobr04@gmail.com>
2022-02-15 09:38:42 -0300

    Criação swagger para documentação da API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import {traderRoutes} from './routes/trades.routes';
 
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/trades",traderRoutes);
 
